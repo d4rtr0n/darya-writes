03b66d29d2540593740121e0ec49b383aadf7055
Darya Danesh <daryac.danesh@gmail.com>
2016-11-08 11:42:55 +0100

    still playing with pop up - tried using a jquery command but it's not working? am i doing jquery wrong? or does mailchimp just fucking hate me? I think the latter.......I almost typed ladder.. it's one of THOSE days....

--- dw.js
+++ dw.js
@@ -1,3 +1,6 @@
+$(document.ready(require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us14.list-manage.com","uuid":"0114e9a368cbacb01ba4f96ed","lid":"68157c6da9"}) }))
+
+
 // function for twitter widget
 window.twttr = (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0],
