f664a7d0efad87dc74e421809c611c3f47722531
Darya Danesh <daryac.danesh@gmail.com>
2016-11-08 12:11:09 +0100

    trying to make the sidebar work better using padding instead of br tags and using align-content but it's not working like i want it to...

--- dw.js
+++ dw.js
@@ -18,3 +18,7 @@ window.twttr = (function(d, s, id) {
   return t;
 }(document, "script", "twitter-wjs"));
 
+
+
+//function for subscription form in sidebar
+(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
